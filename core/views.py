0e20dd44806b6d309bbc2e1d0d53695026ab3d30
Razane1414 <razanefeggous@gmail.com>
2025-12-04 16:33:18 +0100

    upload qui fonctionne avec retour chaine de charactere

--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .forms import UploadImageForm
 
-# Create your views here.
 def home(request):
-    return render(request, 'core/home.html')
+    image_url = None
+    result = None
+
+    if request.method == "POST":
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data["image"]
+
+            fs = FileSystemStorage()
+            filename = fs.save(image.name, image)
+            image_url = fs.url(filename)
+
+            result = "upload fonctionne bien, prochainement : appel IA"
+    else:
+        form = UploadImageForm()
+
+    return render(request, "core/home.html", {"form": form, "image_url": image_url, "result": result})
